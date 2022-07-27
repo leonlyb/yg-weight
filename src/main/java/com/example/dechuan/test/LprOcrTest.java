3412ad6676a534f0b9f5d5f33ea0f73064a1705b
EdenX12 <xxj@dechuan.com>
2022-07-27 13:34:32 +0800

    车牌识别配置文件

--- src/main/java/com/example/dechuan/test/LprOcrTest.java
+++ src/main/java/com/example/dechuan/test/LprOcrTest.java
@@ -4,6 +4,7 @@ import com.example.dechuan.controller.job.LPR;
 import com.example.dechuan.model.ocrlrp.PlateId;
 import org.bytedeco.opencv.opencv_core.Mat;
 
+import java.io.InputStream;
 import java.util.List;
 
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
@@ -16,7 +17,11 @@ import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
  */
 public class LprOcrTest {
     public static void main(String[] args) {
-        LPR lpr = new LPR(false, "");
+        String model = "lpr/10086.pt";
+        InputStream path = Thread.currentThread().getContextClassLoader().getResourceAsStream(model);
+        System.out.println(path);
+
+/*        LPR lpr = new LPR(false, "");
         Mat src = imread("D:\\java\\worktest\\20220720\\MZE120.jpg");
         List<PlateId> ids =lpr.find(src);
         if (ids != null && ids.size() > 0) {
@@ -24,7 +29,7 @@ public class LprOcrTest {
                 String car_id = ids.get(i).getId();
                 System.out.println("id=" + car_id + "  val=" + ids.get(i).getProbabilitie());
             }
-        }
+        }*/
     }
 
 }
