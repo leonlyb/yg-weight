21516ecb01660273664c02e1d037217a6a7fd3ec
eden <879662653@qq.com>
2022-09-30 14:49:20 +0800

    优化

--- src/main/java/com/example/dechuan/hktv/basics/HikVisionService.java
+++ src/main/java/com/example/dechuan/hktv/basics/HikVisionService.java
@@ -192,8 +192,8 @@ public class HikVisionService {
                                 //出厂摄像头
                                 if (ip.equals("192.168.1.240")) {
                                     //根据车牌去查询工单并关闭工单
-                                    logger.info("这是一个出厂摄像头");
-//                                    CarNoImage.closeworkorder(carno);
+                                    logger.info("这是一个出厂摄像头:"+ip);
+                                    CarNoImage.closeworkorder(carno);
                                     }else {
                                         paramMap.put("plateNumber", carno);//车牌号
                                         paramMap.put("byCountry", srt3.substring(1, 2).trim());//省份
